6c226cafa5fc35d350a48fec1cb9e4f1fd295771
john00neill <john2984@live.com>
2021-04-16 16:25:56 +0000

    Removed error on media attribute

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -71,7 +71,6 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.contrib.messages.context_processors.media',
             ],
         },
     },
