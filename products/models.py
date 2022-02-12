ee9c30459c2bad1b1e77eb4aec953e33056b3b64
john00neill <john2984@live.com>
2022-02-12 12:31:12 +0000

    all products in views.py initially updated in products views

--- products/models.py
+++ products/models.py
@@ -19,7 +19,6 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
-    name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
@@ -28,5 +27,6 @@ class Product(models.Model):
 
 
     def __str__(self):
-        return self.name
+        return models.CharField(max_length=254)
+
 
