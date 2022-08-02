dd7c2f2368f7a900eef29d9eb1cf61026eaf84ec
john00neill <john2984@live.com>
2022-08-02 21:23:00 +0000

    Sorting part 1 complete for price rating category on all products

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
 
 # Create your views here.
@@ -50,7 +51,7 @@ def all_products(request):
         'current_categories': categories,
         'current_sorting': current_sorting,
     }
-    
+ 
     return render(request, 'products/products.html', context)
 
 
